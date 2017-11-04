df4e9ea336d76de6f25321f900aa2ac7af691c99
михаил юрков <mishayur1997@mail.ru>
2017-11-04 20:18:47 +0500

    Добавил функции и их производные, удалил старые методы

--- Iterations/Form1.cs
+++ Iterations/Form1.cs
@@ -173,11 +173,6 @@ namespace SLAU
             return sum;
         }
 
-        public void Delta(double i)
-        {
-            strList.Add("delta = " + Convert.ToString(i));
-        }
-
         //Для умножения
         public double Sum(int i, int j, double[,] A, double[,] B)
         {
@@ -269,82 +264,28 @@ namespace SLAU
         }
 
         //Для функции
-        public double[] f_13(double[] x)
+        public double f_13(double x)
         {
-            var temp = new double[n];
-            temp[0] = Math.Sin(x[1]) + 2 * x[0] - 2;
-            temp[1] = Math.Cos(x[0]-1)+x[1]-0.7;            
-            return temp;
+            return Math.Pow(3, x) - 3 * x + 2;
         }
 
-        public double[,] f_derivative_13(double[] x)
+        public double f_derivative_13(double x)
         {
-            var temp = new double[n, n];
-            temp[0, 0] = 2;
-            temp[0, 1] = Math.Cos(x[1]);
-            temp[1, 0] = -Math.Sin(x[0]-1);
-            temp[1, 1] = 1;
-            return temp;
+            return (Math.Pow(3, x) * Math.Log(3) - 3);
         }
 
-        public double[] f_derivative_13_grad(double[] x_)
-        {
-            double x=x_[0], y=x_[1];
-            var temp = new double[n];
-            temp[0] = 4*(2*x+Math.Sin(y)-2)+2*Math.Sin(1-x)*(Math.Cos(1-x)+y-0.7);
-            temp[1] = 2*(Math.Cos(1-x)+y-0.7)+2*Math.Cos(y)*(2*x+Math.Sin(y)-2);
-            return temp;
+       
+        public double f_22(double x)
+        {            
+            return Math.Atan(x) + 2 * x - 1;
         }
 
-        public double[] f_22(double[] x)
+        public double f_derivative_22(double x)
         {
-            var temp = new double[n];
-            temp[0] = Math.Cos(x[0]-1) + x[1] - 0.8;
-            temp[1] = -Math.Cos(x[1]) + x[0] - 2;
-            return temp;
+            return (1 / (x * x + 1) + 2);
         }
 
-        public double[,] f_derivative_22(double[] x)
-        {
-            var temp = new double[n, n];
-            temp[0, 0] = -Math.Sin(x[0] - 1);
-            temp[0, 1] = 1;
-            temp[1, 0] = Math.Sin(x[1]);
-            temp[1, 1] = 1;
-            return temp;
-        }
-
-        public double f_13_grad(double[] x_)
-        {
-
-            double temp,x,y;
-            x = x_[0];
-            y = x_[1];
-            temp = 4 * x * x + 4 * x * (Math.Sin(y) - 2) + Math.Pow(Math.Sin(y), 2) - 4 * Math.Sin(y) + 4.99 + 2 * y * Math.Cos(1 - x) + Math.Pow(Math.Cos(1 - x), 2) - 1.4 * Math.Cos(1 - x) + y * y - 1.4 * y;      
-            return temp;
-        }
-
-        public double f_22_grad(double[] x_)
-        {
-
-            double temp, x, y;
-            x = x_[0];
-            y = x_[1];
-            temp = x * x - 2 * x * Math.Cos(y) + 2 * y * Math.Cos(1 - x) - 4 * x + Math.Pow(Math.Cos(1 - x), 2) - 1.6 * Math.Cos(1 - x) + y * y - 1.6 * y + Math.Pow(Math.Cos(y), 2) + 4 * Math.Cos(y) + 4.64;
-            return temp;
-        }
-
-        public double[] f_derivative_22_grad(double[] x_)
-        {
-            double x, y;
-            x = x_[0];
-            y = x_[1];
-            var temp = new double[n];
-            temp[0] = 2 * (x - Math.Cos(y) - 2) + 2 * Math.Sin(1 - x) * (Math.Cos(1 - x) + y - 0.8);
-            temp[1] = 2 * (Math.Cos(1 - x) + y - 0.8) + 2 * Math.Sin(y) * (x - Math.Cos(y) - 2);
-            return temp;
-        }
-
+       
         public double[] f_13_reverse(double[] x_)
         {
             var temp = new double[n];
@@ -361,215 +302,29 @@ namespace SLAU
             temp[0] = 2 + Math.Cos(x_[1]);
             temp[1] = 0.8 - Math.Cos(x_[0] - 1);
             return temp;
-        }
-
-        //Метод простых итераций
-        public void SimpleIter()
-        {
-            double[] x_new, x_old, pogreshnost;
-            x_new = new double[n];
-            x_old = new double[n];
-            var T = 0.99;
-            pogreshnost = new double[n];
-            double delta = 0;
-            int k = 0;
-            strList.Add("Метод простых итераций:");
-            if (rb_13.Checked)
-            {
-                x_old[0] = 1;
-                x_old[1] = 0;
-                strList.Add("Начальные значения: Х = " + x_old[0] + " Y = " + x_old[1]);
-                strList.Add(String.Format("|  №  |          x           |           y          |         норма        |           q          |", "№", "x1", "y"));
-                strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|", k, x_old[0], x_old[1], delta));
-                do
-                {
-                    k++;
-                    for (int i = 0; i < n; i++)
-                        //x_new[i] = x_old[i] - Multiplication(T, f_13(x_old))[i];
-                        x_new[i] = f_13_reverse(x_old)[i];
-                    for (int i = 0; i < n; i++)
-                        pogreshnost[i] = Math.Abs(x_new[i] - x_old[i]);
-                    delta = Norma(pogreshnost);
-                    x_old = (double[])x_new.Clone();
-                    strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|", k, x_old[0], x_old[1], delta));
-                }
-                while (delta > E);
-            }
-            else
-            {
-                x_old[0] = 2;
-                x_old[1] = 1;
-                strList.Add("Начальные значения: Х = " + x_old[0] + " Y = " + x_old[1]);
-                strList.Add(String.Format("|  №  |          x           |           y          |         норма        |           q          |", "№", "x1", "y"));
-                strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|", k, x_old[0], x_old[1], delta));
-                do
-                {
-                    for (int i = 0; i < n; i++)
-                        //x_new[i] = x_old[i] - Multiplication(T, f_22(x_old))[i];
-                        x_new[i] = f_22_reverse(x_old)[i];
-                    for (int i = 0; i < n; i++)
-                        pogreshnost[i] = Math.Abs(x_new[i] - x_old[i]);
-                    delta = Norma(pogreshnost);
-                    x_old = (double[])x_new.Clone();
-                    strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|", k, x_old[0], x_old[1], delta));
-                    k++;
-                }
-                while (delta > E);
-                b = (double[])x_old.Clone();
-            }
-        }
-
+        }    
+        
         //Метод Ньютона
         public void Niuton()
         {          
-            double[] x_new, x_old,pogreshnost;
-            x_new = new double[n];
-            x_old = new double[n];
-            
-            pogreshnost = new double[n];
-            double delta = 0, stop = 0;
-            int k = 0;
+           
             strList.Add("");
             strList.Add("Метод Ньютона:");           
             if (rb_13.Checked)
             {
-                x_old[0] = 1;
-                x_old[1] = 0;
-                strList.Add("Уравнения вида f1 = sin(y) + 2x - 2; f2= cos(x-1) + y - 0.7");
-                strList.Add("Матрица производных :     2      cos(y)");
-                strList.Add("                      -sin(x-1)     1  ");
-                strList.Add("Начальные значения: Х = " + x_old[0] + " Y = " + x_old[1]);
-                strList.Add(String.Format("|  №  |        x          |         y         |        норма      |         q         |", "№", "x1", "y"));
-                strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|", k, x_old[0], x_old[1], delta));
-                do
-                {
-                    k++;
-                    for (int i = 0; i < n; i++)
-                        x_new[i] = x_old[i] - Multiplication(Reverse(f_derivative_13(x_old)), f_13(x_old))[i];
-                    for (int i = 0; i < n; i++)
-                        pogreshnost[i] = Math.Abs(x_new[i] - x_old[i]);
-                    delta = Norma(pogreshnost);
-                    stop = Norma(Subtraction(f_13(x_new), f_13(x_old)));    
-                    x_old = (double[])x_new.Clone();
-                    strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|", k, x_old[0], x_old[1], delta));
-                }
-                while (delta > E||stop>E);
-                b = (double[])x_old.Clone();
+               
             }
             else
             {
-                x_old[0] = 2;
-                x_old[1] = 0;
-                strList.Add("Уравнения вида f1 = cos(x-1) + y - 0.8; f2= x - cos(y) - 2");
-                strList.Add("Матрица производных : -sin(x-1)   1");
-                strList.Add("                        sin(y)    1");
-                strList.Add("Начальные значения: Х = " + x_old[0] + " Y = " + x_old[1]);
-                strList.Add(String.Format("|  №  |        x          |         y         |        норма      |         q         |", "№", "x1", "y"));
-                strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|", k, x_old[0], x_old[1], delta));
-                do
-                {
-                    for (int i = 0; i < n; i++)
-                        x_new[i] = x_old[i] - Multiplication(Reverse(f_derivative_22(x_old)), f_22(x_old))[i];
-                    for (int i = 0; i < n; i++)
-                        pogreshnost[i] = Math.Abs(x_new[i] - x_old[i]);
-                    delta = Norma(pogreshnost);
-                    stop = Norma(Subtraction(f_22(x_new), f_22(x_old)));
-                    x_old = (double[])x_new.Clone();
-                    strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|", k, x_old[0], x_old[1], delta));
-                    k++;
-                }
-                while (delta > E || stop > E);
-                b = (double[])x_old.Clone();
+                
             }              
         }
 
-        //Метод градиента
-        public void Grad()
-        {
-            double[] x_new, x_old, pogreshnost;
-            x_new = new double[n];
-            x_old = new double[n];
-            x_old[0] = 1;
-            x_old[1] = 0;
-            pogreshnost = new double[n];
-            double delta = 0;
-            double stop,lambda;
-            double Ak;
-            int k = 0;
-            strList.Add("");
-            strList.Add("Метод градиентного спуска:");
-            if (rb_13.Checked)
-            {                
-                lambda = 0.5;               
-                k = 0;
-                strList.Add("Вид минимизируемой функции: 4x^2+4x(sin(y)-2)+(sin(y))^2-4sin(y)+4.99+2ycos(1-x)+(cos(1-x))^2-1.4cos(1-x)+y^2-1.4y");
-                x_old[0] = 1;
-                x_old[1] = 0;
-                Ak = 27.1;                
-                strList.Add("Начальные значения: Х = " + x_old[0] + " Y = " + x_old[1]+" лямбда = "+ lambda+ " альфа = 27.1");
-
-                strList.Add(String.Format("|  №  |         x            |         y            |         норма        |        альфа         |", "№", "x", "y"));
-                strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|{4,22}|", k, x_old[0], x_old[1], delta, Ak));
-                do
-                {
-                    k++;
-                    var f_proizv = new double[n];
-                    Ak = 27.1;
-                    while (f_13_grad(Subtraction(x_old, Multiplication(Ak, f_derivative_13_grad(x_old)))) >= f_13_grad(x_old))
-                        Ak = lambda * Ak;
-                    f_proizv = f_derivative_13_grad(x_old);
-                    f_proizv = Multiplication(Ak, f_proizv);
-                    for (int i = 0; i < n; i++)
-                        x_new[i] = x_old[i] - f_proizv[i];
-                    for (int i = 0; i < n; i++)
-                        pogreshnost[i] = Math.Abs(x_new[i] - x_old[i]);
-                    delta = Norma(pogreshnost);
-                    stop = Math.Abs(f_13_grad(x_new) - f_13_grad(x_old));
-                    x_old = (double[])x_new.Clone();
-                    strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|{4,22}|", k, x_old[0], x_old[1], delta, Ak));
-
-                }
-                while (stop > E || delta > E);
-            }
-            else
-            {                             
-                k=0;
-                lambda = 0.5;
-                x_old[0] = 2;
-                x_old[1] = 0;
-                strList.Add("Вид минимизируемой функции: 4x^2+4x(sin(y)-2)+(sin(y))^2-4sin(y)+4.99+2ycos(1-x)+(cos(1-x))^2-1.4cos(1-x)+y^2-1.4y");
-                strList.Add("Начальные значения: Х = " + x_old[0] + " Y = " + x_old[1] + " лямбда = " + lambda+" альфа = 49,9");
-                strList.Add(String.Format("|  №  |         x           |          y          |         норма       |        альфа        |", "№", "x1", "y"));
-                Ak = 49.9;
-                strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|{4,22}|", k, x_old[0], x_old[1], delta, Ak));
-                do
-                {
-                    k++;
-                    Ak = 49.9;
-                    while (f_22_grad(Subtraction(x_old, Multiplication(Ak, f_derivative_22_grad(x_old)))) >= f_22_grad(x_old))
-                        Ak = lambda * Ak;
-                    var f_proizv = new double[n];
-                    f_proizv = f_derivative_22_grad(x_old);
-                    f_proizv = Multiplication(Ak, f_proizv);
-                    for (int i = 0; i < n; i++)
-                        x_new[i] = x_old[i] - f_proizv[i];
-                    for (int i = 0; i < n; i++)
-                        pogreshnost[i] = Math.Abs(x_new[i] - x_old[i]);
-                    delta = Norma(pogreshnost);
-                    stop = Math.Abs(f_22_grad(x_new) - f_22_grad(x_old));
-                    x_old = (double[])x_new.Clone();
-                    strList.Add(String.Format("|{0,4} |{1,22}|{2,22}|{3,22}|{4,22}|", k, x_old[0], x_old[1], stop, Ak));                    
-                }
-                while (stop > E || delta > E);
-            }
-        }
-
+        
         //Решение уравнения
         private void SolveButton_Click(object sender, EventArgs e)
         {
-            SimpleIter();
-            Niuton();
-            Grad();
+            Niuton();            
             SaveFile();
         }
 
